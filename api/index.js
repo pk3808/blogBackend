730d571693faecbe5365c89ffcb14e0e1c7d5021
Piyush <piyush@ibarts.in>
2025-06-26 18:53:59 +0530

    second commit

--- api/index.js
+++ api/index.js
@@ -26,12 +26,11 @@ app.get('/test', (req, res) => {
   res.send('The API is working fine!');
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 // Export the Express app for Vercel serverless function
 module.exports = (req, res) => {
